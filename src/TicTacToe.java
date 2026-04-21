5ebea790f42fc4532d1537e1d093a05abe3dba3b
vk1700 <Varunkarunakaran10@gmail.com>
2026-04-21 13:24:25 +0530

    UC2: Random player selection and symbol assignment

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,10 +1,16 @@
+import java.util.Random;
+
 public class TicTacToe {
 
     private char[][] board;
+    private char currentPlayer;
+    private char player1Symbol;
+    private char player2Symbol;
 
     public TicTacToe() {
         board = new char[3][3];
         initializeBoard();
+        tossPlayer(); // UC2 logic
     }
 
     private void initializeBoard() {
@@ -12,6 +18,24 @@ public class TicTacToe {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
             }
+        }
+    }
+
+    // UC2: Random toss logic
+    private void tossPlayer() {
+        Random rand = new Random();
+        int result = rand.nextInt(2); // 0 or 1
+
+        if (result == 0) {
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+            currentPlayer = player1Symbol;
+            System.out.println("Player 1 starts with X");
+        } else {
+            player1Symbol = 'O';
+            player2Symbol = 'X';
+            currentPlayer = player2Symbol;
+            System.out.println("Player 2 starts with X");
         }
     }
 
