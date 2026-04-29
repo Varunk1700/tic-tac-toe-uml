7e82c13d44a03e1a39a7ce4004ad95bee9fe3024
vk1700 <Varunkarunakaran10@gmail.com>
2026-04-29 12:56:59 +0530

    UC7: Computer random valid move

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
+import java.util.Random;
+
 public class Main {
 
-    public static void placeMove(char[][] board, int row, int col, char symbol) {
-        board[row][col] = symbol;
-    }
-
+    // Print board
     public static void printBoard(char[][] board) {
         for (int i = 0; i < 3; i++) {
             System.out.println(board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
+        }
+    }
+
+    // UC7: Computer move
+    public static void computerMove(char[][] board) {
+        Random rand = new Random();
+
+        while (true) {
+            int slot = rand.nextInt(9) + 1;
+
+            int row = (slot - 1) / 3;
+            int col = (slot - 1) % 3;
+
+            if (board[row][col] == ' ') {
+                board[row][col] = 'O';
+                break;
+            }
         }
     }
 
@@ -18,8 +34,8 @@ public class Main {
             {' ', ' ', ' '}
         };
 
-        // Example move
-        placeMove(board, 1, 1, 'X');
+        // Computer plays
+        computerMove(board);
 
         printBoard(board);
     }
